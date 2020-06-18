0d662a0e8be7363e63fef01b1e7225b5153d35c7
Dave Coss <cossack001@gmail.com>
2020-06-18 15:27:10 +0800

    Initial Commit

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,11 +7,9 @@ export const Footer = styled(({ ...styleProps }) => {
   return (
     <footer {...styleProps}>
       <Wrapper>
-        © {new Date().getFullYear()} – Built with
+        © {new Date().getFullYear()} – 
         {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a> and{" "}
-        <a href="https://www.tinacms.org">Tina</a> <span>by </span>
-        <a href="https://www.forestry.io">Forestry.io</a>
+        <a href="https://www.northshoredialadriver.co.nz">North Shore Dial a Driver</a>
       </Wrapper>
     </footer>
   )
